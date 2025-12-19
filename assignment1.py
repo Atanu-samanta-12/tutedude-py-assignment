0f9ed20009729fd5addcacd84e1e51db56fa6aff
Atanu-samanta-12 <atanusamanta283@gmail.com>
2025-12-19 12:53:15 +0530

    small changes

--- assignment1.py
+++ assignment1.py
@@ -1,19 +1,19 @@
-# # task 1 
-# a = float(input("Enter the 1st number: ", ))
-# b = float(input("Enter the 2nd number: ", ))
+# task 1 
+a = float(input("Enter the 1st number: ", ))
+b = float(input("Enter the 2nd number: ", ))
 
-# add = a+b 
+add = a+b 
 
-# if a>b:
-#     sub = a-b
-# else:
-#     sub = b-a
+if a>b:
+    sub = a-b
+else:
+    sub = b-a
 
-# multiplication = a*b 
+multiplication = a*b 
 
-# division = a/b
+division = a/b
 
-# print(f"Addition:{add}\nSubtraction:{sub}\nMultiplication:{multiplication}\nDivision:{division}")
+print(f"Addition:{add}\nSubtraction:{sub}\nMultiplication:{multiplication}\nDivision:{division}")
 
 # task 2
 
