644789808527acdd35038c2b265c8f3840e8f2a6
Atanu-samanta-12 <atanusamanta283@gmail.com>
2025-12-19 12:51:32 +0530

    complete both task

---
+++ assignment1.py
@@ -0,0 +1,23 @@
+# # task 1 
+# a = float(input("Enter the 1st number: ", ))
+# b = float(input("Enter the 2nd number: ", ))
+
+# add = a+b 
+
+# if a>b:
+#     sub = a-b
+# else:
+#     sub = b-a
+
+# multiplication = a*b 
+
+# division = a/b
+
+# print(f"Addition:{add}\nSubtraction:{sub}\nMultiplication:{multiplication}\nDivision:{division}")
+
+# task 2
+
+first_name = input("Enter Your First Name: ", )
+last_name = input("Enter Your Last Name: ", ) 
+full_name = first_name+" "+last_name
+print(f"Hello! {full_name}. Its really nice to meet you.")
